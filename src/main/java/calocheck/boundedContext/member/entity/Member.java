055c9b082433fbd273b27b6b8a1253f435847008
깃waimi3169 <waimi3169@naver.com>
2023-06-21 16:07:29 +0900

    Feat: 회원속성 추가

--- src/main/java/calocheck/boundedContext/member/entity/Member.java
+++ src/main/java/calocheck/boundedContext/member/entity/Member.java
@@ -1,6 +1,7 @@
 package calocheck.boundedContext.member.entity;
 
 import calocheck.base.entity.BaseEntity;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,4 +14,19 @@ import lombok.experimental.SuperBuilder;
 @SuperBuilder(toBuilder = true)
 @ToString(callSuper = true)
 public class Member extends BaseEntity {
+    private String providerTypeCode; // 일반/소셜 중 무엇으로 가입했는지 확인용
+    @Column(unique = true)
+    private String username;
+    private String password;
+    private String name;
+
+    private String email;
+
+    @Column(unique = true)
+    private String nickname;
+    private int age;
+    private double height;
+    private double weight;
+    private double muscleMass;
+    private double bodyFat;
 }
