e2a17ed2101aa8a6f99be49de417e3e27c207c62
Park0720 <ppmm98@naver.com>
2023-06-22 13:41:57 +0900

    feat: Comment CRUD

--- src/main/java/calocheck/boundedContext/comment/controller/CommentController.java
+++ src/main/java/calocheck/boundedContext/comment/controller/CommentController.java
@@ -1,14 +1,63 @@
 package calocheck.boundedContext.comment.controller;
 
 import calocheck.base.rq.Rq;
+import calocheck.base.rsData.RsData;
+import calocheck.boundedContext.comment.entity.Comment;
 import calocheck.boundedContext.comment.service.CommentService;
+import calocheck.boundedContext.post.service.PostService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 
 @Controller
 @RequiredArgsConstructor
 public class CommentController {
     private final Rq rq;
     private final CommentService commentService;
+    private final PostService postService;
 
+    @PreAuthorize("isAuthenticated()")
+    @PostMapping("/post/{postId}/comment")
+    public String saveComment(@PathVariable Long postId, String iComment) {
+        if (iComment == null || iComment.length() == 0) {
+            return rq.historyBack("댓글을 입력해주세요.");
+        }
+
+        RsData<Comment> commentRsData =
+                commentService.saveComment(
+                        iComment,
+                        postService.findById(postId).get(),
+                        rq.getMember()
+                );
+
+        if (commentRsData.isFail()) {
+            return rq.historyBack(commentRsData);
+        }
+
+        return rq.redirectWithMsg("/post/" + postId, commentRsData);
+    }
+
+    @PreAuthorize("isAuthenticated()")
+    @PatchMapping("/post/{postId}/comment/{commentId}")
+    public String deleteComment(@PathVariable Long postId, @PathVariable Long commentId) {
+        RsData<Comment> deleteCmtRsData = commentService.deleteComment(commentId, rq.getMember().getId());
+
+        return rq.redirectWithMsg("/post/" + postId, deleteCmtRsData);
+    }
+
+    @PreAuthorize("isAuthenticated()")
+    @PatchMapping("/post/{postId}/comment/{commentId}/modify")
+    public String modifyComment(
+            @PathVariable Long postId,
+            @PathVariable Long commentId,
+            String modifyComment
+    ) {
+        RsData<Comment> modifyCommentRsData =
+                commentService.modifyComment(commentId, modifyComment, rq.getMember());
+
+        return rq.redirectWithMsg("/post/" + postId, modifyCommentRsData);
+    }
 }
--- src/main/java/calocheck/boundedContext/comment/repository/CommentRepository.java
+++ src/main/java/calocheck/boundedContext/comment/repository/CommentRepository.java
@@ -3,5 +3,10 @@ package calocheck.boundedContext.comment.repository;
 import calocheck.boundedContext.comment.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+    Optional<Comment> findById(Long id);
+    List<Comment> findAllByPostId(Long postId);
 }
