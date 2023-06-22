3f18682c774ff2d2ae3c948eafa9bdd4ed3d2a1e
Park0720 <ppmm98@naver.com>
2023-06-22 22:54:52 +0900

    Chore: member, Post testdata

--- src/main/java/calocheck/base/initData/NotProd.java
+++ src/main/java/calocheck/base/initData/NotProd.java
@@ -1,23 +1,40 @@
 package calocheck.base.initData;
 
+import calocheck.boundedContext.member.entity.Member;
+import calocheck.boundedContext.member.service.MemberService;
+import calocheck.boundedContext.post.entity.Post;
+import calocheck.boundedContext.post.service.PostService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.core.annotation.Order;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.stream.IntStream;
 
 
 @Configuration
-@Profile({"test"})
+@Profile({"dev", "test"})
 public class NotProd {
     @Bean
-    @Order(1)
-    CommandLineRunner initData() {
-        return new CommandLineRunner() {
-            @Override
-            public void run(String... args) throws Exception {
+    @Transactional
+    public CommandLineRunner initData(
+            MemberService memberService,
+            PostService postService
+    ) {
+        return args -> {
+            Member[] members = IntStream
+                    .rangeClosed(1, 10)
+                    .mapToObj(i -> memberService.join("user%d".formatted(i), "1234", null,
+                                    "닉네임%d".formatted(i), null, null, null, null, null)
+                            .getData())
+                    .toArray(Member[]::new);
 
-            }
+            Post[] posts = IntStream
+                    .rangeClosed(1, 100)
+                    .mapToObj(i -> postService.savePost("%d번 글입니다.".formatted(i), "%d번 내용입니다.".formatted(i), members[i % 10])
+                            .getData())
+                    .toArray(Post[]::new);
         };
     }
 }
