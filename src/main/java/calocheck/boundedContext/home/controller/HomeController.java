05384eaa535c7febbfc1a3a93455a1fe51f51c22
seongmin8636 <moss3004@gmail.com>
2023-06-22 15:20:24 +0900

    Feat : íheader,footer,config 기본 추가

---
+++ src/main/java/calocheck/boundedContext/home/controller/HomeController.java
@@ -0,0 +1,13 @@
+package calocheck.boundedContext.home.controller;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class HomeController {
+    @GetMapping("/")
+    public String showMain() { return null;}
+}
