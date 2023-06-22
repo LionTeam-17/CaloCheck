17290017e0bd8ef6a9a15b8523fef5a4bfa0fc3a
seongmin8636 <moss3004@gmail.com>
2023-06-22 15:23:14 +0900

    Fix : home 컨트롤러 수정

--- src/main/java/calocheck/boundedContext/home/controller/HomeController.java
+++ src/main/java/calocheck/boundedContext/home/controller/HomeController.java
@@ -8,6 +8,4 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 @RequiredArgsConstructor
 public class HomeController {
-    @GetMapping("/")
-    public String showMain() { return null;}
 }
