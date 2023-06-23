90bf2cd9dd8b042221dcc32cfa499ef572948c6e
Park0720 <ppmm98@naver.com>
2023-06-23 12:58:37 +0900

    feat: Post UI연결

--- src/main/java/calocheck/boundedContext/post/controller/PostController.java
+++ src/main/java/calocheck/boundedContext/post/controller/PostController.java
@@ -7,12 +7,14 @@ import calocheck.boundedContext.comment.service.CommentService;
 import calocheck.boundedContext.post.entity.Post;
 import calocheck.boundedContext.post.service.PostService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/post")
@@ -22,35 +24,50 @@ public class PostController {
     private final PostService postService;
     private final CommentService commentService;
 
-    @GetMapping("/")
-    public String showPostList(Model model) {
+    @GetMapping("")
+    public String showPostList(@RequestParam(defaultValue = "0") int page, Model model) {
         List<Post> postList = postService.findAll();
+        Page<Post> paging = postService.getList(page);
+
         model.addAttribute("postList", postList);
+        model.addAttribute("paging", paging);
 
         return "usr/post/list";
     }
+
     @GetMapping("/{postId}")
     public String showPostPage(Model model, @PathVariable Long postId) {
+        Optional<Post> oPost = postService.findById(postId);
+        oPost.ifPresent(post -> model.addAttribute("post", post));
+
         List<Comment> commentList = commentService.findAllByPostId(postId);
         model.addAttribute("commentList", commentList);
 
-        return "usr/post/post" + postId;
+        return "usr/post/post";
     }
 
-    @PreAuthorize("isAuthenticated()")
-    @PostMapping("/{postId}")
-    public String savePost(@PathVariable Long postId, String subject, String content) {
-        if (subject == null || subject.length() == 0) {
+    // @PreAuthorize("isAuthenticated()")
+    // todo 로그인 연결 시 주석 해제
+    @GetMapping("/createForm")
+    public String savePost() {
+        return "usr/post/createForm";
+    }
+
+    // @PreAuthorize("isAuthenticated()")
+    // todo 로그인 연결 시 주석 해제
+    @PostMapping("/createForm")
+    public String savePost(String iSubject, String iContent) {
+        if (iSubject == null || iSubject.length() == 0) {
             return rq.historyBack("제목을 입력해주세요.");
         }
-        if (content == null || content.length() == 0) {
+        if (iContent == null || iContent.length() == 0) {
             return rq.historyBack("내용을 입력해주세요.");
         }
 
         RsData<Post> postRsData =
                 postService.savePost(
-                        subject,
-                        content,
+                        iSubject,
+                        iContent,
                         rq.getMember()
                 );
 
@@ -58,9 +75,10 @@ public class PostController {
             return rq.historyBack(postRsData);
         }
 
-        return rq.redirectWithMsg("/post/" + postId, postRsData);
+        return rq.redirectWithMsg("/post", postRsData);
     }
 
+    // todo UI와 연결
     @PreAuthorize("isAuthenticated()")
     @PatchMapping("/{postId}")
     public String deletePost(@PathVariable Long postId) {
@@ -69,6 +87,7 @@ public class PostController {
         return rq.redirectWithMsg("/postId/" + postId, deletePostRsData);
     }
 
+    // todo UI와 연결
     @PreAuthorize("isAuthenticated()")
     @PatchMapping("/{postId}/modify")
     public String modifyPost(@PathVariable Long postId, String modifyPost) {
