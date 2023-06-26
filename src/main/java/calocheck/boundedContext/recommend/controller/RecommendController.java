97cfaff9d69ef1a0688df38ab31d924613bf3380
SeoMoonk <39723465+SeoMoonk@users.noreply.github.com>
2023-06-26 13:35:51 +0900

    Enhancement/27 (#28)
    Co-authored-by: 깃SeoMoonK <깃허sunghoon1216@naver.com>

--- src/main/java/calocheck/boundedContext/recommend/controller/RecommendController.java
+++ src/main/java/calocheck/boundedContext/recommend/controller/RecommendController.java
@@ -1,19 +1,44 @@
 package calocheck.boundedContext.recommend.controller;
 
+import calocheck.base.rsData.RsData;
+import calocheck.boundedContext.photo.service.PhotoService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/recommend")
 public class RecommendController {
 
+    private final PhotoService photoService;
+
     @GetMapping("/list")
     public String getRecommendList(){
 
         return "/usr/food/recommendList";
+    }
+
+
+    //FIXME 임시(다른 위치로 이동 필요)
+    @PostMapping("/img")
+    public String uploadImg(@RequestParam(required = false) MultipartFile img) throws IOException {
+
+        RsData<String> isImg = photoService.isImgFile(img.getOriginalFilename());
+
+        String photoUrl = "not image file";
+
+        if(isImg.isSuccess()){
+            //DB에 이미지 경로 저장 가능
+            photoUrl = photoService.photoUpload(img);
+        }else{
+            System.out.println(isImg.getMsg());
+        }
+
+        return "redirect:/recommend/list";
     }
 
 
