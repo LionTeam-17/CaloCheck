4342591d23bc14316d9e6ec98326f4b9a9cf29d2
seongmin8636 <moss3004@gmail.com>
2023-06-29 17:53:02 +0900

    Feat : 헤더 링크 연결

--- src/main/java/calocheck/boundedContext/tracking/controller/TrackingController.java
+++ src/main/java/calocheck/boundedContext/tracking/controller/TrackingController.java
@@ -1,5 +1,6 @@
 package calocheck.boundedContext.tracking.controller;
 
+import calocheck.base.rq.Rq;
 import calocheck.boundedContext.member.entity.Member;
 import calocheck.boundedContext.member.service.MemberService;
 import calocheck.boundedContext.tracking.entity.Tracking;
@@ -24,26 +25,27 @@ public class TrackingController {
 
     private final TrackingService trackingService;
     private final MemberService memberService;
+    private final Rq rq;
 
     @Autowired
-    public TrackingController(TrackingService trackingService, MemberService memberService) { // Inject MemberService
-        this.trackingService = trackingService;
-        this.memberService = memberService;
+    public TrackingController(Rq rq, TrackingService trackingService, MemberService memberService) {
+            this.rq =rq;
+            this.trackingService = trackingService;
+            this.memberService = memberService;
     }
 
-    @GetMapping("/tracking/{memberId}")
-    public String showTracking(@PathVariable("memberId") Long memberId, Model model) {
-        Optional<Member> memberOptional = memberService.findById(memberId);
+    @GetMapping("/tracking")
+    public String showTracking(Model model) {
+        Optional<Member> memberOptional = memberService.findById(rq.getMember().getId());
 
         if (!memberOptional.isPresent()) {
-            // Handle error. For example, you can return a error message or error page.
             return "error/memberNotFound";
         }
 
         Member member = memberOptional.get();
         List<Tracking> trackingData = trackingService.findTrackingsByMember(member);
-        model.addAttribute("trackingData", trackingData); // Add trackingData to the model
-        model.addAttribute("tracking", new Tracking()); // Add an empty Tracking object to the model
+        model.addAttribute("trackingData", trackingData);
+        model.addAttribute("tracking", new Tracking());
         return "usr/tracking/bodyTracking";
     }
 
@@ -51,20 +53,17 @@ public class TrackingController {
     public String createTracking(@ModelAttribute("tracking")
                                  @DateTimeFormat(pattern = "yyyy-MM-dd") Tracking tracking,
                                  Principal principal) {
-        // Retrieve the logged in member
+
         String currentPrincipalName = principal.getName();
         Member member = memberService.findByUsername(currentPrincipalName)
                 .orElseThrow(() -> new UsernameNotFoundException("User Not Found with username: " + currentPrincipalName));
 
-        // Set member in tracking data
         tracking.setMember(member);
 
-        // Check if tracking data for the given date already exists
         Optional<Tracking> existingTracking = trackingService.findByMemberAndDate(member, tracking.getDateTime());
 
         Tracking savedTracking;
         if (existingTracking.isPresent()) {
-            // Update existing tracking data
             Tracking trackingToUpdate = existingTracking.get();
             trackingToUpdate.setWeight(tracking.getWeight());
             trackingToUpdate.setBodyFat(tracking.getBodyFat());
@@ -72,12 +71,10 @@ public class TrackingController {
 
             savedTracking = trackingService.updateTracking(trackingToUpdate);
         } else {
-            // Save new tracking data
             savedTracking = trackingService.createTracking(tracking);
         }
 
-        // Redirect to tracking page
-        return "redirect:/tracking/" + savedTracking.getMember().getId();
+        return "redirect:/tracking";
     }
 
 }
