5c5397f4382b729c152fc54ac2a0daff88243061
waimi3169 <125889390+waimi3169@users.noreply.github.com>
2023-06-30 20:39:35 +0900

    Feat: 내 정보 페이지에서 내가 쓴 목록 표시, 회원가입 디폴트 값 변경 (#71)
    * 회원가입 버튼 추가
    
    * Feat: 내 정보 페이지 구현
    
    * Fix : 오타 수정
    
    * Feat: 닉네임 수정 테스트
    
    * Feat: 닉네임 수정 가능
    
    * Feat: 회원가입 폼 형식 변경
    
    * Feat: 신체정보 수정 기능
    
    * Feat: 충돌해결2
    
    * Feat: 내정보 박스 추가
    
    * Feat: 정보ã 수정 폼
    
    * Feat: 불필요한 코드 제거
    
    * Feat: 정보 입력란 단위 추가
    
    * Fix : 회원의 고유id 수정페이지와 연결 안되는 버그 해결
    
    * Feat: 이메일 수정 가능
    
    * Feat: 내가 쓴 글 목록 페이징처리 코드 구현
    
    * Feat: Re 페이징처리 글목록 코딩
    
    * Feat: 페이지 버튼 추가 , 제목 클릭하면 해당 게시물 페이지로 이동
    
    * Feat: 회원 게시물 목록 ui
    
    * Feat: 내 정보 박스도 ui 적용
    
    * Feat: 회원가입 시 선택사항 입력 안할 시 default ê값 = 0
    
    ---------
    
    Co-authored-by: 깃waimi3169 <깃허waimi3169@naver.com>

--- src/main/java/calocheck/boundedContext/member/controller/MemberController.java
+++ src/main/java/calocheck/boundedContext/member/controller/MemberController.java
@@ -7,6 +7,8 @@ import calocheck.boundedContext.member.service.MemberService;
 import calocheck.boundedContext.tracking.entity.Tracking;
 import calocheck.boundedContext.tracking.service.TrackingService;
 import calocheck.boundedContext.post.entity.Post;
+import calocheck.boundedContext.post.repository.PostRepository;
+import calocheck.boundedContext.post.service.PostService;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
@@ -14,11 +16,18 @@ import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.time.LocalDate;
 
 @Controller
@@ -27,10 +36,13 @@ import java.time.LocalDate;
 public class MemberController {
     private final MemberService memberService;
     private final Rq rq;
+    private final PostService postService;
+    private final PostRepository postRepository;
     private final TrackingService trackingService;
 
     @AllArgsConstructor
     @Getter
+    @Setter
     public static class JoinForm {
         @NotBlank
         @Size(min = 4, max = 20)
@@ -65,6 +77,26 @@ public class MemberController {
 
         if (checkRsData.isFail()) {
             return rq.historyBack(checkRsData.getMsg());
+        }
+
+        if (joinForm.getAge() == null) {
+            joinForm.setAge(0);
+        }
+
+        if (joinForm.getHeight() == null) {
+            joinForm.setHeight(0.0);
+        }
+
+        if (joinForm.getWeight() == null) {
+            joinForm.setWeight(0.0);
+        }
+
+        if (joinForm.getMuscleMass() == null) {
+            joinForm.setMuscleMass(0.0);
+        }
+
+        if (joinForm.getBodyFat() == null) {
+            joinForm.setBodyFat(0.0);
         }
 
         RsData<Member> joinRs = memberService.join(
@@ -106,7 +138,8 @@ public class MemberController {
             return rq.historyBack(modifyRsData);
         }
 
-        return rq.redirectWithMsg("/member/mypage", modifyRsData);
+        return rq.redirectWithMsg("/member/mypage/{memberId}", modifyRsData);
+      
     }
 
 
@@ -151,10 +184,34 @@ public class MemberController {
 
         return ResponseEntity.ok().body("{\"message\": \"이메일이 %s(으)로 수정되었습니다.\"}".formatted(email));
     }
-
     @PreAuthorize("isAuthenticated()")
-    @GetMapping("/mypage")
-    public String showMyPage() {
+    @GetMapping("/mypage/{id}")
+    public String showMyPage(
+            Model model,
+            @PathVariable Long id,
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "5") int size
+    ) {
+        // 회원 정보를 가져옵니다.
+        Member member = memberService.findById(id).orElse(null);
+
+        // 회원이 작성한 모든 글을 조회합니다.
+        List<Post> allPosts = member.getPosts();
+
+        int totalPosts = allPosts.size();
+        int totalPages = (int) Math.ceil((double) totalPosts / size);
+
+        // 페이징 처리를 위한 글 목록을 구합니다.
+        int startIdx = page * size;
+        int endIdx = Math.min(startIdx + size, allPosts.size());
+        List<Post> pagedPosts = allPosts.subList(startIdx, endIdx);
+
+        // 조회된 글 목록을 모델에 추가합니다.
+        model.addAttribute("postList", pagedPosts);
+
+        model.addAttribute("page", page);
+        model.addAttribute("size", size);
+        model.addAttribute("totalPages", totalPages);
 
         return "usr/member/mypage";
     }
