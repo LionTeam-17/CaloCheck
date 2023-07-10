f196c9e7dc5f93d503849691d058cec6f2f3411a
seongmin8636 <moss3004@gmail.com>
2023-07-10 14:12:16 +0900

    Fix : NotProd Tracking 주석 해제

--- src/main/java/calocheck/base/initData/NotProd.java
+++ src/main/java/calocheck/base/initData/NotProd.java
@@ -12,6 +12,7 @@ import calocheck.boundedContext.tag.service.TagService;
 import calocheck.boundedContext.post.service.PostService;
 import calocheck.boundedContext.postLike.service.PostLikeService;
 import calocheck.boundedContext.recommend.service.RecommendService;
+import calocheck.boundedContext.tracking.entity.Tracking;
 import calocheck.boundedContext.tracking.service.TrackingService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
@@ -19,6 +20,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDate;
+import java.util.Random;
 import java.util.stream.IntStream;
 
 @Configuration
@@ -85,28 +88,28 @@ public class NotProd {
 //                            .getData())
 //                    .toArray(PostLike[]::new);
 //
-//            //Tracking 샘플 데이터
-//            LocalDate startDate = LocalDate.now().minusDays(90);
-//            Random random = new Random();
-//
-//            for (Member member : members) {
-//                int age = member.getAge();
-//                double height = member.getHeight();
-//                double weight = 60 + random.nextDouble() * 5;
-//                double bodyFat = 20 + random.nextDouble() * 5;
-//                double muscleMass = 30 + random.nextDouble() * 5;
-//                LocalDate date = startDate;
-//
-//                while (!date.isAfter(LocalDate.now())) {
-//                    weight = Math.round((weight + (random.nextDouble() * 6) - 3) * 10) / 10.0;
-//                    bodyFat = Math.round((bodyFat + (random.nextDouble() * 0.2) - 0.1) * 10) / 10.0;
-//                    muscleMass = Math.round((muscleMass + (random.nextDouble() * 0.3) - 0.15) * 10) / 10.0;
-//                    Tracking tracking = trackingService.createTracking(member, date, age, height, weight, bodyFat, muscleMass, null, null);
-//                    trackingService.calculateBMI(tracking);
-//                    trackingService.calculateBodyFatPercentage(tracking);
-//                    date = date.plusDays(random.nextInt(4) + 1);
-//                }
-//            }
+            //Tracking 샘플 데이터
+            LocalDate startDate = LocalDate.now().minusDays(90);
+            Random random = new Random();
+
+            for (Member member : members) {
+                int age = member.getAge();
+                double height = member.getHeight();
+                double weight = 60 + random.nextDouble() * 5;
+                double bodyFat = 20 + random.nextDouble() * 5;
+                double muscleMass = 30 + random.nextDouble() * 5;
+                LocalDate date = startDate;
+
+                while (!date.isAfter(LocalDate.now())) {
+                    weight = Math.round((weight + (random.nextDouble() * 6) - 3) * 10) / 10.0;
+                    bodyFat = Math.round((bodyFat + (random.nextDouble() * 0.2) - 0.1) * 10) / 10.0;
+                    muscleMass = Math.round((muscleMass + (random.nextDouble() * 0.3) - 0.15) * 10) / 10.0;
+                    Tracking tracking = trackingService.createTracking(member, date, age, height, weight, bodyFat, muscleMass, null, null);
+                    trackingService.calculateBMI(tracking);
+                    trackingService.calculateBodyFatPercentage(tracking);
+                    date = date.plusDays(random.nextInt(4) + 1);
+                }
+            }
 
 
         };
