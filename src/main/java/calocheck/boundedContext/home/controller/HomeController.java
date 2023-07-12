4297ad1964b90b67d7b030cf6cbf7429670d1974
SeoMoonk <sunghoon1216@naver.com>
2023-07-12 17:01:49 +0900

    Fix: json print

--- src/main/java/calocheck/boundedContext/home/controller/HomeController.java
+++ src/main/java/calocheck/boundedContext/home/controller/HomeController.java
@@ -1,15 +1,32 @@
 package calocheck.boundedContext.home.controller;
 
+import calocheck.base.util.s3.service.S3Service;
+import calocheck.boundedContext.imageData.config.GCPConfigProperties;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 @Controller
 @RequiredArgsConstructor
 public class HomeController {
-    
+
+    private final S3Service s3Service;
+    private final GCPConfigProperties gcpConfigProperties;
+
     @GetMapping("/")
-    public String showMain() {
+    public String showMain() throws IOException {
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(s3Service.getFileFromS3(gcpConfigProperties.getFilePath())));
+        String line;
+        while ((line = reader.readLine()) != null) {
+            System.out.println(line);
+        }
+
 
         return "usr/home/main";
     }
