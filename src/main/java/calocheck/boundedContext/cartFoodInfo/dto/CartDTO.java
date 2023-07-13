0664456b1380505e0099a0942cc98ce58bc012dc
박진용 <70837543+CatJelly@users.noreply.github.com>
2023-07-13 13:04:56 +0900

    feat: 식단리스트 수정, 삭제 (#188)
    * fix: 식품 관련 페이지 폰트 크기 수정
    
    * fix: 식품 관련 페이지 폰트 크기 수정
    
    * feat: 장바구니 통합 추가 처리
    
    * feat: addMenu 중복 항목 처리
    
    * feat: 식단 기록 현재 식단 출력
    
    * feat: 버튼 rounded 통일
    
    * fix: 장바구니 이름 변경
    
    * feat: 식단 스크롤 처리
    
    * feat: 식단이 있는 날 표시
    
    * fix: total, list 페이지 통합
    
    * feat: 섭취 정도 세분화
    
    * fix: 헤더 아이콘 정렬 및 크기 수정
    
    * feat: 장바구니 삭제 후 처리1
    
    * fix: conflict fix
    
    * fix: conflict fix
    
    * fix: conflict fix
    
    * feat: 식단리스트 수정, 삭제 차트 반영
    
    * fix: 홈 맵핑

--- src/main/java/calocheck/boundedContext/cartFoodInfo/dto/CartDTO.java
+++ src/main/java/calocheck/boundedContext/cartFoodInfo/dto/CartDTO.java
@@ -1,12 +1,17 @@
 package calocheck.boundedContext.cartFoodInfo.dto;
 
+import calocheck.boundedContext.nutrient.dto.NutrientDTO;
 import lombok.Data;
+
+import java.util.List;
 
 @Data
 public class CartDTO {
     private String result;
     private String msg;
     private Long deletedId;
+    private Double totalKcal;
+    private List<NutrientDTO> nutrients;
 
     public CartDTO(String result, String msg) {
         this.result = result;
@@ -17,5 +22,20 @@ public class CartDTO {
         this.result = result;
         this.msg = msg;
         this.deletedId = deletedId;
+    }
+
+    public CartDTO(String result, String msg, Double totalKcal, List<NutrientDTO> nutrients) {
+        this.result = result;
+        this.msg = msg;
+        this.totalKcal = totalKcal;
+        this.nutrients = nutrients;
+    }
+
+    public CartDTO(String result, String msg, Long deletedId, Double totalKcal, List<NutrientDTO> nutrients) {
+        this.result = result;
+        this.msg = msg;
+        this.deletedId = deletedId;
+        this.totalKcal = totalKcal;
+        this.nutrients = nutrients;
     }
 }
